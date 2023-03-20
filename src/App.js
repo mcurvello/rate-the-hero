30a41bf75320dfd5ae6825dca4e3b1b737217e69
Marcio Curvello <marciocurvello@take.net>
2023-03-19 21:17:39 -0300

    Upload inicial dos arquivos principais do projeto

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import { Details } from "./screens/Details";
+import { Search } from "./screens/Search";
 
-function App() {
+export function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <BrowserRouter>
+        <Switch>
+          <Route path="/detalhes/:id" exact>
+            <Details />
+          </Route>
+          <Route path="/" exact>
+            <Search />
+          </Route>
+          <Route path="*">Página não encontrada</Route>
+        </Switch>
+      </BrowserRouter>
+    </>
   );
 }
-
-export default App;
